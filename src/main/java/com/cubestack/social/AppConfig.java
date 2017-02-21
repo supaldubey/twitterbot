e7fcd793bde6b44241ce8e8281d417d4efdc7c2e
Supal Dubey <supal.dubey@gmail.com>
2017-02-21 21:48:33 +0530

    MySQL migration

--- src/main/java/com/cubestack/social/AppConfig.java
+++ src/main/java/com/cubestack/social/AppConfig.java
@@ -3,8 +3,6 @@
  */
 package com.cubestack.social;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -24,12 +22,6 @@ import twitter4j.TwitterFactory;
 @EnableTransactionManagement
 public class AppConfig {
 
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/console/*");
-		return registration;
-	}
 
 	@Bean
 	public Twitter twitter() {
